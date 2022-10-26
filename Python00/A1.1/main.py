4f71a8ceaeb4a617b8a599fbb8ab25569e629509
DanielSaGo <114920927+DanielSaGo@users.noreply.github.com>
2022-10-26 13:28:54 +0200

    A1.1

---
+++ Python00/A1.1/main.py
@@ -0,0 +1,13 @@
+# Este es el Ejercicio 1.1 de Python
+
+numero = int(input())
+if numero >= 9 and numero <= 10:
+    print("A")
+elif numero >= 8 and numero < 9:
+    print("B")
+elif numero >= 7 and numero < 8:
+    print("C")
+elif numero >= 6 and numero < 7:
+    print("D")
+elif numero >= 0 and numero < 6:
+    print("F")
